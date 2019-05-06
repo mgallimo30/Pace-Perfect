68605cc8bf615bc08a413eb1e8f53cd55f5cd410
Matt <mgallimo@me.com>
2019-05-06 15:56:28 -0400

    Final Commit

--- src/main/kotlin/Timing.kt
+++ src/main/kotlin/Timing.kt
@@ -1,3 +1,6 @@
+import tornadofx.*
+import kotlin.properties.Delegates
+
 /*
  * Name        : "Pace Perfect"
  * Project     : Timing_App_Logic
@@ -7,43 +10,184 @@
  * License     : Copyright (C) 2019 All rights reserved
  */
 
-class timing {
+fun main() {
+    val t = Timing()
+    t.goalDistance = 5.0
+    t.selectedDistanceUnit = "Kilometers"
+    t.goalMinute = 20
+    t.selectedSplitType = "Distance"
+    t.splitNum = 1.0
+    t.selectedSplitUnit = "Miles"
+    t.calculateSplits()
+}
+
+class Splits {
+    var unit: String by Delegates.observable("") {_, oldValue, newValue ->
+        onUnitChanged?.invoke(oldValue, newValue)
+    }
+    private var onUnitChanged: ((String, String) -> Unit)? = null
+
+    var distance: Double by Delegates.observable(0.0){ _, oldValue, newValue ->
+        onDistanceChanged?.invoke(oldValue, newValue)
+    }
+    private var onDistanceChanged: ((Double, Double) -> Unit)? = null
+
+    var hour: Int by Delegates.observable(0){ _, oldValue, newValue ->
+        onHourChanged?.invoke(oldValue, newValue)
+    }
+    private var onHourChanged: ((Int, Int) -> Unit)? = null
+
+    var minute: Int by Delegates.observable(0){ _, oldValue, newValue ->
+        onMinuteChanged?.invoke(oldValue, newValue)
+    }
+    private var onMinuteChanged: ((Int, Int) -> Unit)? = null
+
+    var second: Double by Delegates.observable(0.0){ _, oldValue, newValue ->
+        onSecondChanged?.invoke(oldValue, newValue)
+    }
+    private var onSecondChanged: ((Double, Double) -> Unit)? = null
+}
+
+class Timing {
 
     // Converted JAVA Logic
     // set global variables to be used by all functions
-    val total_distance        = 0.0                       //total distance
-    val total_time_s          = 0                         //in total secs
-    val total_pace_s          = 0                         //in total secs
-    val time_hour             = 0                         //cd ..4hr component for time
-    val time_minute           = 0                         //min component for time
-    val time_second           = 0                         //sec component for time
-    val pace_hour             = 0                         //hr component for pace
-    val pace_minute           = 0                         //min component for pace
-    val pace_second           = 0                         //sec component for pace
-    val marathon_distance     = "26.21875"                //marathon distance in miles
-    val half_marathon_distance= "13.109375"               //half marathon distance in miles
+    private var totalDistance = 0.0                       //total distance
+    var goalDistance = 0.0                       //goal distance
+    private var totalTimeS = 0.0                       //in total secs
+    private var totalPaceS = 0.0                         //in total secs
+    var goalHour = 0                         //cd ..4hr component for time
+    var goalMinute = 0                         //min component for time
+    var goalSecond = 0.0                       //sec component for time
+    private var paceHour = 0                         //hr component for pace
+    private var paceMinute = 0                         //min component for pace
+    private var paceSecond = 0.0                         //sec component for pace
+    val marathonDistance = "26.21875"                //marathon distance in miles
+    val halfMarathonDistance = "13.109375"               //half marathon distance in miles
+    var splitList = mutableListOf<Splits>().observable()
+    var selectedDistanceUnit = ""
+    var selectedSplitType = ""
+    var splitNum = 0.0
+    var selectedSplitUnit = ""
 
-    enum class pace_unit {                      //unit pace is in (the per unit, aka mile, kilometer, quarter, half, etc)
-        mile, kilometer, meter
-    }
-    enum class distance_unit {                  //type of unit dist is miles or kilometers
-        Miles,
-        Kilometers
-    }
-    enum class event {                          //event distance like marathon, half-marathon
-        Marathon,
-        Halfmarathon,
-        FiveK,
-        FiveM,
-        EightK,
-        TenK,
-        FifteenK,
-        TenM,
-        TwentyK,
-        FifteenM,
-        TwentyfiveK,
-        ThirtyK,
-        TwentyM
+    fun calculateSplits() {
+        goalTimeToSeconds()
+        convertDistanceType()
+        calculatePace()
     }
 
+    fun reset(){
+        totalDistance = 0.0                       //total distance
+        goalDistance = 0.0                       //goal distance
+        totalTimeS = 0.0                       //in total secs
+        totalPaceS = 0.0                         //in total secs
+        goalHour = 0                         //cd ..4hr component for time
+        goalMinute = 0                         //min component for time
+        goalSecond = 0.0                       //sec component for time
+        paceHour = 0                         //hr component for pace
+        paceMinute = 0                         //min component for pace
+        paceSecond = 0.0                         //sec component for pace
+        splitList.clear()
+        selectedDistanceUnit = ""
+        selectedSplitType = ""
+        splitNum = 0.0
+        selectedSplitUnit = ""
+    }
+
+    private fun goalTimeToSeconds() {
+        totalTimeS += goalHour * 3600
+        totalTimeS += goalMinute * 60
+        totalTimeS += goalSecond
+    }
+
+    private fun convertDistanceType() {
+        if (selectedDistanceUnit == "Miles" && selectedSplitUnit == "Miles"
+            && selectedSplitType == "Distance"
+        ) {
+            //todo remove if not used
+        } else if (selectedDistanceUnit == "Miles" && selectedSplitUnit == "Kilometers"
+            && selectedSplitType == "Distance"
+        ) {
+            goalDistance *= 1.60934
+        } else if (selectedDistanceUnit == "Miles" && selectedSplitUnit == "Meters"
+            && selectedSplitType == "Distance"
+        ) {
+            goalDistance *= 1609.34
+        } else if (selectedDistanceUnit == "Kilometers" && selectedSplitUnit == "Miles"
+            && selectedSplitType == "Distance"
+        ) {
+            goalDistance *= 0.621371
+        } else if (selectedDistanceUnit == "Kilometers" && selectedSplitUnit == "Kilometers"
+            && selectedSplitType == "Distance"
+        ) {
+            //todo remove if not used
+        } else if (selectedDistanceUnit == "Kilometers" && selectedSplitUnit == "Meters"
+            && selectedSplitType == "Distance"
+        ) {
+            goalDistance *= 1000
+        } else if (selectedDistanceUnit == "Meters" && selectedSplitUnit == "Miles"
+            && selectedSplitType == "Distance"
+        ) {
+            goalDistance *= 0.000621371
+        } else if (selectedDistanceUnit == "Meters" && selectedSplitUnit == "Kilometers"
+            && selectedSplitType == "Distance"
+        ) {
+            goalDistance *= 0.001
+        } else if (selectedDistanceUnit == "Meters" && selectedSplitUnit == "Meters"
+            && selectedSplitType == "Distance"
+        ) {
+            //todo remove if not used
+        }
+    }
+
+    private fun calculatePace() {
+        do {
+            totalPaceS = totalTimeS
+            totalPaceS /= goalDistance
+            if (goalDistance >= splitNum) {
+                totalDistance += splitNum
+                goalDistance -= splitNum
+                totalPaceS *= splitNum
+                totalTimeS -= totalPaceS
+                paceSecond += totalPaceS % 60
+                if (paceSecond >= 60) {
+                    paceSecond %= 60
+                    paceMinute++
+                }
+                totalPaceS /= 60
+                paceMinute += (totalPaceS % 60).toInt()
+                if (paceMinute >= 60) {
+                    paceMinute %= 60
+                    paceHour++
+                }
+                totalPaceS /= 60
+                paceHour += totalPaceS.toInt()
+            } else {
+                totalDistance += goalDistance
+                goalDistance -= splitNum
+                paceSecond += totalPaceS % 60
+                if (paceSecond >= 60) {
+                    paceSecond %= 60
+                    paceMinute++
+                }
+                totalTimeS /= 60
+                paceMinute += (totalTimeS % 60).toInt()
+                if (paceMinute >= 60) {
+                    paceMinute %= 60
+                    paceHour++
+                }
+                totalTimeS /= 60
+                paceHour += totalTimeS.toInt()
+            }
+            val splits = Splits()
+            splits.unit = selectedSplitUnit
+            splits.distance = totalDistance
+            splits.hour = paceHour
+            splits.minute = paceMinute
+            splits.second = paceSecond
+            splitList.add(splits)
+            //println("$selectedSplitUnit $totalDistance: $paceHour:$paceMinute:$paceSecond") //todo
+
+        } while (goalDistance > 0)
+    }
 }
--- src/main/kotlin/source.kt
+++ src/main/kotlin/source.kt
@@ -1,5 +1,3 @@
-import javax.swing.text.View
-
 /*
  * Name        : "Pace Perfect"
  * Project     : Timing_App_Logic
@@ -9,8 +7,304 @@ import javax.swing.text.View
  * License     : Copyright (C) 2019 All rights reserved
  */
 
-fun main(){
+package com.example.app.myapp
 
-    println("Probably the best timing app in the world")
+import Splits
+import Timing
+import javafx.beans.property.*
+import javafx.scene.paint.Color
+import tornadofx.*
 
+val t = Timing()
+
+class MyApp : App(MyView::class)
+
+class MyView : View("Pace Perfect") {
+    private val controller: MyController by inject()
+    private var selectedEvent = SimpleStringProperty()
+    private val goalDistanceField = SimpleDoubleProperty()
+    private var selectedDistance = SimpleStringProperty()
+    private val goalHourField = SimpleIntegerProperty()
+    private val goalMinuetField = SimpleIntegerProperty()
+    private val goalSecondField = SimpleDoubleProperty()
+    private var selectedSplitType = SimpleStringProperty()
+    private val splitValue = SimpleDoubleProperty()
+    private var selectedSplitDistance = SimpleStringProperty()
+    private var error = false
+    private val distanceUnit = observableList(
+        "Miles",
+        "Kilometers",
+        "Meters"
+    )
+    private val splits = observableList(
+        //"Time",  //todo
+        "Distance"
+    )
+    private val event = observableList(
+        "",
+        "Marathon",
+        "Half Marathon",
+        "5K",
+        "5 Miles",
+        "8K",
+        "10K",
+        "15K",
+        "10 Miles",
+        "20K",
+        "15 Miles",
+        "25K",
+        "30K",
+        "20 Miles"
+    )
+
+    override val root = borderpane {
+        setPrefSize(414.0, 736.0)
+        center = form {
+            useMaxWidth = true
+            style {
+                backgroundColor += Color.BLACK
+            }
+
+
+        }
+    }
+
+    init {
+        with(root.center)
+        {
+            vbox {
+                label("Please enter goal distance, goal time, and splits") {
+                    textFill = Color.WHITE
+                    style {
+                        fontSize = 18.px
+                    }
+                }
+                label("Select an event or enter goal distance") {
+                    textFill = Color.WHITE
+                    style {
+                        fontSize = 15.px
+                    }
+                }
+                label("Event ") {
+                    textFill = Color.WHITE
+                }
+                combobox(selectedEvent, event)
+                label("Goal Distance") {
+                    textFill = Color.WHITE
+                }
+                hbox {
+                    textfield(goalDistanceField)
+                    combobox(selectedDistance, distanceUnit)
+                }
+                label("Please enter goal time") {
+                    textFill = Color.WHITE
+                    style {
+                        fontSize = 18.px
+                    }
+                }
+                label("Hours") {
+                    textFill = Color.WHITE
+                }
+                textfield(goalHourField)
+                label("Minutes:") {
+                    textFill = Color.WHITE
+                }
+                textfield(goalMinuetField)
+                label("Seconds:") {
+                    textFill = Color.WHITE
+                }
+                textfield(goalSecondField)
+                label("Split:") {
+                    textFill = Color.WHITE
+                }
+                hbox {
+                    combobox(selectedSplitType, splits)
+                    textfield(splitValue)
+                    combobox(selectedSplitDistance, distanceUnit)
+                }
+                label()
+                label("Hit save to show splits or reset to clear form") {
+                    textFill = Color.WHITE
+                    style {
+                        fontSize = 18.px
+                    }
+                }
+                hbox {
+                    button("Save") {
+                        useMaxWidth = true
+                        action {
+                            if (goalDistanceField.value != 0.0
+                                && selectedDistance.value != null
+                                && (goalHourField.value != 0
+                                        || goalMinuetField.value != 0
+                                        || goalSecondField.value != 0.0)
+                                && selectedSplitType.value != null
+                                && splitValue.value != 0.0
+                                && selectedSplitDistance.value != null
+                            ) {
+                                controller.storeData(
+                                    goalDistanceField.value, goalHourField.value, goalMinuetField.value,
+                                    goalSecondField.value, splitValue.value
+                                )
+                                controller.calcSplits()
+                                selectedEvent.value = null
+                                goalDistanceField.value = 0.0
+                                selectedDistance.value = null
+                                goalHourField.value = 0
+                                goalMinuetField.value = 0
+                                goalSecondField.value = 0.0
+                                selectedSplitType.value = null
+                                splitValue.value = 0.0
+                                selectedSplitDistance.value = null
+                                replaceWith<Watch>()
+                            } else {
+                                error = true
+                            }
+                        }
+                    }
+                    button("Reset") {
+                        useMaxWidth = true
+                        action {
+                            selectedEvent.value = null
+                            goalDistanceField.value = 0.0
+                            selectedDistance.value = null
+                            goalHourField.value = 0
+                            goalMinuetField.value = 0
+                            goalSecondField.value = 0.0
+                            selectedSplitType.value = null
+                            splitValue.value = 0.0
+                            selectedSplitDistance.value = null
+                        }
+                    }
+
+                }
+            }
+        }
+        selectedEvent.onChange {
+            when (it) {
+                "Marathon" -> {
+                    goalDistanceField.value = t.marathonDistance.toDouble()
+                    selectedDistance.value = "Miles"
+                }
+                "Half Marathon" -> {
+                    goalDistanceField.value = t.halfMarathonDistance.toDouble()
+                    selectedDistance.value = "Miles"
+                }
+                "5K" -> {
+                    goalDistanceField.value = 5.0
+                    selectedDistance.value = "Kilometers"
+                }
+                "5 Miles" -> {
+                    goalDistanceField.value = 5.0
+                    selectedDistance.value = "Miles"
+                }
+                "8K" -> {
+                    goalDistanceField.value = 5.0
+                    selectedDistance.value = "Kilometers"
+                }
+                "10K" -> {
+                    goalDistanceField.value = 10.0
+                    selectedDistance.value = "Kilometers"
+                }
+                "15K" -> {
+                    goalDistanceField.value = 15.0
+                    selectedDistance.value = "Kilometers"
+                }
+                "10 Miles" -> {
+                    goalDistanceField.value = 10.0
+                    selectedDistance.value = "Miles"
+                }
+                "20K" -> {
+                    goalDistanceField.value = 20.0
+                    selectedDistance.value = "Kilometers"
+                }
+                "15 Miles" -> {
+                    goalDistanceField.value = 15.0
+                    selectedDistance.value = "Miles"
+                }
+                "25K" -> {
+                    goalDistanceField.value = 25.0
+                    selectedDistance.value = "Kilometers"
+                }
+                "30K" -> {
+                    goalDistanceField.value = 30.0
+                    selectedDistance.value = "Kilometers"
+                }
+                "20 Miles" -> {
+                    goalDistanceField.value = 20.0
+                    selectedDistance.value = "Miles"
+                }
+            }
+        }
+
+        selectedDistance.onChange {
+            if (it != null) {
+                controller.storeDistanceUnit(it)
+            }
+        }
+
+        selectedSplitType.onChange {
+            if (it != null) {
+                controller.storeSelectedSplitType(it)
+            }
+        }
+
+        selectedSplitDistance.onChange {
+            if (it != null) {
+                controller.storeSelectedSplitUnit(it)
+            }
+        }
+    }
+
+}
+
+class Watch : View("Watch") {
+    private val controller: MyController by inject()
+    override val root = borderpane {
+        bottom = hbox {
+            button("Reset") {
+                useMaxWidth = true
+                action {
+                    controller.clearData()
+                    replaceWith<MyView>()
+                }
+            }
+        }
+        center = tableview(t.splitList) {
+            column("Unit", Splits::unit)
+            column("Distance", Splits::distance)
+            column("Hour", Splits::hour)
+            column("Minute", Splits::minute)
+            column("Second", Splits::second)
+        }
+    }
+}
+
+class MyController : Controller() {
+    fun storeData(goalD: Double, goalH: Int, goalM: Int, goalS: Double, split: Double) {
+        t.goalDistance = goalD
+        t.goalHour = goalH
+        t.goalMinute = goalM
+        t.goalSecond = goalS
+        t.splitNum = split
+    }
+
+    fun storeDistanceUnit(unit: String) {
+        t.selectedDistanceUnit = unit
+    }
+
+    fun storeSelectedSplitType(unit: String) {
+        t.selectedSplitType = unit
+    }
+
+    fun storeSelectedSplitUnit(unit: String) {
+        t.selectedSplitUnit = unit
+    }
+
+    fun calcSplits() {
+        t.calculateSplits()
+    }
+    fun clearData() {
+        t.reset()
+    }
 }
